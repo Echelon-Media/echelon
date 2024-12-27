9e7bdbe16f0b5ece25aad7d38286255eb89647be
Sachintha-echelon <sachintha@echelon.media>
2024-12-27 14:41:01 +0530

    Removed snowfall

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,22 +3,12 @@ import "@/styles/shine-50/styles.scss";
 import "@/styles/shine-50/homepage.scss";
 
 import Link from "next/link";
-import Snowfall from "react-snowfall";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     
-                  <Snowfall   style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          zIndex: 5000,
-          pointerEvents: 'none', // Optional: to prevent interference with user interactions
-        }} /> 
-
+          
       <Component {...pageProps} />
       {/* <CookieConsent style={{ background: "rgba(53, 53, 53,0.8)" }}>
         ECHELON.lk uses cookies to enhance the user experience. By continuing to
