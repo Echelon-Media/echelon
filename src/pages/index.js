9c97cc6ddd41b61ce96bc74645ee2bcfe69f1bd4
Sachintha-echelon <sachintha@echelon.media>
2024-12-30 14:25:55 +0530

    update laoding time

--- src/pages/index.js
+++ src/pages/index.js
@@ -270,7 +270,7 @@ export default function Home() {
  useEffect(() => {
     const timer = setTimeout(() => {
       onLoadComplete();
-    },2000); 
+    },1000); 
     return () => clearTimeout(timer);
   }, []);
 
